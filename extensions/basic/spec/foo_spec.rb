fd5537e4d46a62a1ab53e8d59e2719fd12a87b04
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-27 23:48:58 +0200

    Added comments and clarifications to the different classes and specs.

--- extensions/basic/spec/foo_spec.rb
+++ extensions/basic/spec/foo_spec.rb
@@ -2,15 +2,23 @@ require "spec_helper"
 
 ##
 # Describe behaviour created in the JRuby extension
-# Module named Foo. This class include common basic
-# things you can do with modules as in JRuby Extensions
-# mecanisms.
+# module named Foo. This class include common basic
+# things you can do in the JRuby extension mechanism.
 ##
 describe Foo do
 
-  context "simple static methods" do
+  context "static methods" do
     it 'shout return a hash with a hostname' do
       expect(subject.default_options).to include( "hostname" => "example.org" )
+    end
+  end
+
+  context "instance methods" do
+
+    subject(:foo) { FooClass.new }
+
+    it "should add two numbers" do
+      expect(foo.add(2,3)).to eq(5)
     end
 
   end
